88c50e3c45b1e8f6b4bada86773bc06fb5d3296a
Tyler Tolley <thattolleyguy@gmail.com>
2013-09-05 21:47:53 -0600

    Upgrading how a defender moves

--- src/com/ciess/node/Defender.java
+++ src/com/ciess/node/Defender.java
@@ -1,27 +1,55 @@
 package com.ciess.node;
 
-import java.util.List;
-
 import com.badlogic.gdx.math.Vector2;
 
 public class Defender {
 	Vector2 location;
 	int currentTarget;
-	List<Vector2> targets;
+	Vector2[] targets;
 	Vector2 velocity;
-	
-	float size;
-	
+	float speed = 0.5f;
+	private final float size;
+
 	public Defender() {
 		location = new Vector2(0, 0);
-		
-		size = 50f;
+		targets = new Vector2[] { new Vector2(20, 20), new Vector2(-20, -20),
+				new Vector2(-20, 20), new Vector2(20, -20) };
+		this.size = 8.0f;
+
+		currentTarget = targets.length;
+		changeTarget();
 	}
-	
-	public void update(float deltaTime)
-	{
+
+	public Defender(Vector2 startingLocation, float size, Vector2... targets) {
+		this.targets = targets;
+		this.location = startingLocation;
+		this.size = size;
+		changeTarget();
+	}
+
+	public float getSize() {
+		return size;
+	}
+
+	public void update(float deltaTime) {
 		location = location.add(velocity);
-		
+		if (hitTarget()) {
+			changeTarget();
+		}
+
 	}
-	
+
+	private void changeTarget() {
+		currentTarget++;
+		if (currentTarget >= targets.length)
+			currentTarget = 0;
+		velocity = targets[currentTarget].cpy().sub(location).nor().scl(speed);
+	}
+
+	private boolean hitTarget() {
+		if ((Math.abs(targets[currentTarget].x - location.x) < size / 2)
+				&& (Math.abs(targets[currentTarget].y - location.y) < size / 2))
+			return true;
+		return false;
+	}
 }
--- src/com/ciess/node/NodeRenderer.java
+++ src/com/ciess/node/NodeRenderer.java
@@ -10,25 +10,27 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 public class NodeRenderer {
 	OrthographicCamera cam;
 	Node node;
+
 	public NodeRenderer(Node node) {
-		
-		cam = new OrthographicCamera(300, 300);
-		cam.position.set(0,0,0);
+
+		cam = new OrthographicCamera(480, 320);
+		cam.position.set(0, 0, 0);
 		this.node = node;
-		
+
 	}
-	public void render (float deltaTime) {
+
+	public void render(float deltaTime) {
 		cam.update();
 		renderDefenders(node.defenders);
 	}
+
 	private void renderDefenders(Collection<Defender> defenders) {
 		ShapeRenderer renderer = new ShapeRenderer();
 		renderer.setProjectionMatrix(cam.combined);
 		renderer.begin(ShapeType.Filled);
 		renderer.setColor(Color.RED);
-		for(Defender d : defenders)
-		{
-			renderer.rect(d.location.x, d.location.y, d.size, d.size);
+		for (Defender d : defenders) {
+			renderer.rect(d.location.x, d.location.y, d.getSize(), d.getSize());
 		}
 		renderer.end();
 	}
